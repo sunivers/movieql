c27d054753ec3366371ee03b986d207743b639ba
soyoung <soyoungp02@gmail.com>
2020-01-01 23:24:39 +0900

    add fetch api & connect movie api

--- graphql/db.js
+++ graphql/db.js
@@ -1,48 +1,17 @@
-let movies = [
-  {
-    id: 1,
-    name: "Avengers",
-    score: 85,
-  },
-  {
-    id: 2,
-    name: "Notebook",
-    score: 90,
-  },
-  {
-    id: 3,
-    name: "Frozen2",
-    score: 92,
-  },
-  {
-    id: 4,
-    name: "Logan",
-    score: 83,
-  }
-];
+import fetch from 'node-fetch';
 
-export const getMovies = () => movies;
+const API_URL = 'https://yts.lt/api/v2/list_movies.json';
+
+export const getMovies = (limit = 0, rating = 0) => 
+{
+  let REQUEST_URL = `${API_URL}?limit=${limit}&minimum_rating=${rating}`;
+  
+  return fetch(REQUEST_URL)
+  .then(res => res.json())
+  .then(json => json.data.movies);
+}
 
 export const getById = id => {
   const filteredMovie = movies.filter(movie => movie.id === id);
   return filteredMovie[0];
-}
-
-export const addMovie = (name, score) => {
-  const newMovie = {
-    id: `${movies.length + 1}`,
-    name,
-    score
-  };
-  movies.push(newMovie);
-  return newMovie;
-}
-
-export const deleteMovie = id => {
-  const cleanedMovies = movies.filter(movie => movie.id !== id);
-  if(movies.length > cleanedMovies.length) {
-    movies = cleanedMovies;
-    return true;
-  }
-  return false;
 }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,13 +1,9 @@
-import { getById, getMovies, addMovie, deleteMovie } from './db.js';
+import { getMovies, getById } from './db.js';
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
+    movies: (_, { limit, rating }) => getMovies(limit, rating),
     movie: (_, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_, { name, score }) => addMovie(name, score),
-    deleteMovie: (_, { id }) => deleteMovie(id)
   }
 };
 
