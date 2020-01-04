228001d8e49df8cf3f417dfdf91c54dd8884608f
soyoung <soyoungp02@gmail.com>
2020-01-04 23:24:27 +0900

    add movie detail api

--- graphql/db.js
+++ graphql/db.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
-const API_URL = 'https://yts.lt/api/v2/list_movies.json';
+const BASE_URL = 'https://yts.lt/api/v2/'
+const LIST_MOVIES_URL = `${BASE_URL}list_movies.json`;
+const MOVIE_DETAIL_URL = `${BASE_URL}movie_details.json`;
 
 export const getMovies = async (limit = 0, rating = 0) => 
 {
-  const { data: { data: { movies } } } = await axios(API_URL, {
+  const { data: { data: { movies } } } = await axios(LIST_MOVIES_URL, {
     params: { limit, minimum_rating: rating }
   });
   return movies;
 }
 
-export const getById = id => {
-  const filteredMovie = movies.filter(movie => movie.id === id);
-  return filteredMovie[0];
+export const getMovie = async id => {
+  const { data: { data: { movie } } } = await axios(MOVIE_DETAIL_URL, {
+    params: { movie_id: id }
+  });
+  return movie;
 }
--- graphql/resolvers.js
+++ graphql/resolvers.js
@@ -1,9 +1,9 @@
-import { getMovies, getById } from './db.js';
+import { getMovies, getMovie } from './db.js';
 
 const resolvers = {
   Query: {
     movies: (_, { limit, rating }) => getMovies(limit, rating),
-    movie: (_, { id }) => getById(id)
+    movie: (_, { id }) => getMovie(id)
   }
 };
 
